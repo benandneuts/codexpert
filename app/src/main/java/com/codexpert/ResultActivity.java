8743618245faefb954228da12eea1c7cf736cc63
bastien lefumeux <bastien.lefumeux@etu.unice.fr>
2022-03-31 12:02:53 +0200

    Ajout de l'interface Score
    -> Objectif : faire le back pour le prochain push

--- app/src/main/java/com/codexpert/ResultActivity.java
+++ app/src/main/java/com/codexpert/ResultActivity.java
@@ -1,21 +1,61 @@
 package com.codexpert;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button share,detail_rep,suite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entrainement);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); // Cette ligne cache la barre implémenté par défaut
+
+        setContentView(R.layout.activity_main);
+
         TextView score = findViewById(R.id.score);
         TextView txtScore = findViewById(R.id.res_txt);
-        Button share = findViewById(R.id.shareButton);
-        Button suite = findViewById(R.id.suivant);
-        Button detail_rep = findViewById(R.id.details);
+        share = findViewById(R.id.shareButton);
+        suite = findViewById(R.id.suite);
+        detail_rep = findViewById(R.id.details);
+
+        score.setText("35/40");
+        txtScore.setText("Félicitation !");
+
+        // click events
+        share.setOnClickListener(this);
+        detail_rep.setOnClickListener(this);
+        suite.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == share) {
+            partagerScore(); // TODO && temp
+        }
+        else if(v == detail_rep) {
+            afficherDetail(); // TODO && temp
+        }
+        else if(v == suite) {
+            afficherSuite(); // TODO && temp
+        }
+    }
+
+    private void afficherSuite() {
+    }
+
+    private void afficherDetail() {
+    }
+
+    private void partagerScore() {
     }
 }
